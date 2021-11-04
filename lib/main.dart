eac020079e2c8fc109bc98c219c6244f90b58b93
komjatia <komjati.akos156@icloud.com>
2021-11-05 00:28:42 +0100

    created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue,
+          appBar: AppBar(title: Text("Magic 8 ball pool"),backgroundColor: Colors.blueGrey,
+          ),
+          body: Magic8Ball(),
+        )
       ),
     );
+class Magic8Ball extends StatefulWidget {
+
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int BallNumber = Random().nextInt(5) + 1;
+  void ChangeBallNumber(){
+    setState(() {
+      BallNumber = Random().nextInt(5) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(
+      child: FlatButton(
+        onPressed: (){ChangeBallNumber();},
+        child: Image.asset('images/ball$BallNumber.png'),
+      ),
+    ),);
+  }
+}
